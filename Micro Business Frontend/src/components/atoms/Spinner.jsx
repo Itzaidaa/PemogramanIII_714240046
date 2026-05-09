ecb321d1ecaa0262800efcae399a27e1a6740022
unknown <itzaidaa@gmail.com>
2026-05-09 22:44:18 +0700

    remove folders

--- Micro Business Frontend/src/components/atoms/Spinner.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Loader2 } from 'lucide-react';
-
-const Spinner = () => <Loader2 className="w-10 h-10 text-[#3B82F6] animate-spin" />;
-
-export default Spinner;
--- Micro Business Frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// API Configuration
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api', // Disesuaikan dengan port backend Node.js
-  timeout: 10000,
-});
-
-export default api;
